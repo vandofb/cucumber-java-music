830050486ba64ed78636fc7f9d5cdb7d18cfe850
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2018-02-05 12:22:42 +0000

    Use JsonReporter12

--- src/test/java/skeleton/RunCukesTest.java
+++ src/test/java/skeleton/RunCukesTest.java
@@ -5,6 +5,6 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"pretty", "io.cucumber.pro.JsonReporter:all"})
+@CucumberOptions(plugin = {"pretty", "io.cucumber.pro.JsonReporter12:full-gradle"})
 public class RunCukesTest {
 }
